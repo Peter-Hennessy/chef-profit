6fa9287c6257460d62f3fa45d8d2c1288a493be2
Peter Hennessy <peternesta@hotmail.com>
2022-04-15 18:45:20 +0000

    Add sales data to worksheet

--- run.py
+++ run.py
@@ -19,10 +19,12 @@ def get_sales_data():
     """
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, and  seperated by commas.")
+        print("Data should be a four didget number, and  seperated by a full stop.")
         print("Example: 10,20,30,40,50,60\n")
     
-        data_str = input("Enter Data Here: ")
+        data_str = input("Enter Selling price Here: ")
+                   
+
 
         sales_data = data_str.split(",")
 
@@ -43,13 +45,26 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values requires, you provided {len(values)}"
+                f"Exactly  values requires, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
 
+
     return True
-    
+
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add row with list of data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales successfuly updated.\n")
+
 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
