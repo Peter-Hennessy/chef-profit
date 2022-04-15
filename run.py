590ff5dda7673fe9af6514c78c967fc6eb288721
Peter Hennessy <peternesta@hotmail.com>
2022-04-15 17:27:02 +0000

    Adjust validatio to check that all values provided are integers

--- run.py
+++ run.py
@@ -33,7 +33,9 @@ def validate_data(values):
     Raises ValueError if the srings cannot be converted into lines,
     or if there aren't exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values requires, you provided {len(values)}"
