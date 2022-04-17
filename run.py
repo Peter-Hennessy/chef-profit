51badb0740664faee4abce479adcb1d1563cb443
Peter Hennessy <peternesta@hotmail.com>
2022-04-17 20:37:42 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -60,7 +60,18 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)	
     print("Sales successfuly updated.\n")
 
-def calculate_suprlus_data(sales_row):
+
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add row with list of data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)	
+    print("Surplus successfuly updated.\n")
+
+def calculate_surplus_data(sales_row):
     """
     Comapre sales with staock and calculayte the surplus for each type item.
 
@@ -71,7 +82,13 @@ def calculate_suprlus_data(sales_row):
     print("Calculating surplus data....\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 def main():
@@ -81,8 +98,10 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_suprlus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(sales_data)
+    print(new_surplus_data)
 
-    
+
 print("Welcome to love sandwiches data automation")
 main()
